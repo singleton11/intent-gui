0839fbbc790dc99cefdad0a2531a3167158bfe50
Anton Prokhorov <betrayer11@gmail.com>
2017-01-27 20:54:41 +0700

    Add bootstrap4 support

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -19,6 +19,10 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
+  app.import('bower_components/bootstrap/dist/css/bootstrap.min.css');
+  app.import('bower_components/jquery/dist/jquery.slim.min.js');
+  app.import('bower_components/tether/dist/js/tether.min.js');
+  app.import('bower_components/bootstrap/dist/js/bootstrap.min.js');
 
   return app.toTree();
 };

7615b7ee8c3c0cbf95783d354330fe3af9bc454e
Anton Prokhorov <betrayer11@gmail.com>
2017-01-28 11:15:59 +0700

    Resolve trailing slash issue

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -3,4 +3,7 @@ import DS from 'ember-data';
 export default DS.JSONAPIAdapter.extend({
   host: 'http://localhost:8000/api',
   namespace: 'projects',
+  buildURL: function (type, id, record) {
+    return this._super(type, id, record) + '/';
+  },
 });

590ae88f438985053e0e1db283e9796598236c22
Anton Prokhorov <betrayer11@gmail.com>
2017-01-29 00:18:43 +0700

    Move saving logic to component to follow DRY

--- app/components/project-form.js
+++ app/components/project-form.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  router: Ember.inject.service('-routing'),
+  actions: {
+    save(changeset) {
+      changeset.save().then((record) => {
+        this.get('router').transitionTo('projects.project.show', [record.id]);
+      }).catch(() => {
+        alert('Server error');
+      });
+    }
+  }
 });

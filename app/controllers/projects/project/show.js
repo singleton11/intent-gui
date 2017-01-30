cd11dd5bca973041a58fe2e6a183715284f87a46
Anton Prokhorov <betrayer11@gmail.com>
2017-01-30 21:13:26 +0700

    Add delete confirmation dialog

--- app/controllers/projects/project/show.js
+++ app/controllers/projects/project/show.js
@@ -2,7 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   actions: {
+    confirm() {
+      $('#confirm').modal('show');
+    },
     remove() {
+      $('#confirm').modal('hide');
       this.get('model').destroyRecord().then(() => {
         this.transitionToRoute('projects');
       })
